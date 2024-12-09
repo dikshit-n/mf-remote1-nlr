2a92cdcf9fa9580e9d1b28b09c8d9e350d76147b
dikshit-n <dikshitkumarn@gmail.com>
2024-12-09 17:52:12 +0530

    props

--- components/AppRemote1.vue
+++ components/AppRemote1.vue
@@ -1,11 +1,11 @@
 <template>
   Remote App 1 - NLR
-  <p>Message: {{ props.msg }}</p>
+  <p>Message: {{ msg }}</p>
 </template>
 
 <script lang="ts">
 import { defineProps, withDefaults } from 'vue'
-  const props = withDefaults(defineProps(), {
+  const props = withDefaults(defineProps<{msg: string}>(), {
     msg: "TEST MESSAGE",
   })
 </script>
