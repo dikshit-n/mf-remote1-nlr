40af8c9afdd82a5748fb5eae6dbace3f655345f4
dikshit-n <dikshitkumarn@gmail.com>
2024-12-13 18:28:49 +0530

    trigger error

--- components/AppRemote1.vue
+++ components/AppRemote1.vue
@@ -1,9 +1,19 @@
 <template>
   Remote App 1 - NLR
   <p>Message: {{ msg }}</p>
+  <BaseButton @click="componentError">Component Error</BaseButton>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue'
 defineProps<{msg: string}>()
+
+const data = reactive({
+    test: {
+      name: 'test'
+    }
+  })
+  const componentError = () => {
+    data.test = undefined
+  }
 </script>
